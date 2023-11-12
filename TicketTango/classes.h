2dce2997e6399ce3121467ed9d6c788848adc95b
cody <coculescuioan22@stud.ase.ro>
2023-11-12 11:59:27 +0200

    Partial public interface implementation

--- TicketTango/classes.h
+++ TicketTango/classes.h
@@ -3,11 +3,68 @@
 #include <iostream>
 #include <string>
 
+class Util {
+public:
+	static char* copyString(const char* source) {
+		if (source == nullptr)
+			return nullptr;
+
+		char* value = new char[strlen(source) + 1];
+		strcpy_s(value, strlen(source) + 1, source);
+
+		return value;
+	}
+};
+
 class Location {
 	std::string name;
 	int capacity = 0; // total number of seats
 	int noRows = 0;
 public:
+	std::string getName() {
+		return this->name;
+	}
+
+	void setName(std::string newName) {
+		if (newName[0] < 'A' || newName[0] > 'Z') {
+			throw std::exception("Name must start with a capital letter");
+		}
+		this->name = newName;
+	}
+
+	int getCapacity() {
+		return this->capacity;
+	}
+
+	void setCapacity(int c) {
+		if (c <= 0) {
+			throw std::exception("Capacity must be strictly positive");
+		}
+		this->capacity = c;
+	}
+
+	int getNoRows() {
+		return this->noRows;
+	}
+
+	void setNoRows(int r) {
+		if (r <= 0) {
+			throw std::exception("Number of rows must be strictly positive");
+		}
+		this->noRows = r;
+	}
+
+	void displayDetails() {
+		std::cout << std::endl << "Location Name: " << this->getName();
+		std::cout << std::endl << "Capacity: " << this->getCapacity();
+		std::cout << std::endl << "Number of Rows: " << this->getNoRows();
+	}
+
+	Location(std::string name, int capacity, int rows) {
+		this->setName(name);
+		this->setCapacity(capacity);
+		this->setNoRows(rows);
+	}
 };
 
 class Event {
@@ -16,12 +73,69 @@ class Event {
 	char time[6] = ""; //	hh:mm
 	static int NO_EVENTS;
 public:
+	std::string getName() {
+		return this->name;
+	}
+
+	void setName(std::string newName) {
+		if (newName[0] < 'A' || newName[0] > 'Z') {
+			throw std::exception("Name must start with a capital letter");
+		}
+		this->name = newName;
+	}
+
+	char* getDate() {
+		return Util::copyString(this->date);
+	}
+
+	void setDate(const char* newDate) {
+		if (strlen(newDate) != 10) {
+			throw std::exception("Wrong date length");
+		}
+		if (newDate[2] != '/' || newDate[5] != '/') {
+			throw std::exception("Wrong date format");
+		}
+
+		strcpy_s(this->date, newDate);
+	}
+
+	char* getTime() {
+		return Util::copyString(this->time);
+	}
+
+	void setTime(const char* newTime) {
+		if (strlen(newTime) != 5) {
+			throw std::exception("Wrong time length");
+		}
+		if (newTime[2] != ':') {
+			throw std::exception("Wrong time format");
+		}
+
+		strcpy_s(this->time, newTime);
+	}
+
+	static int getNoEvents() {
+		return Event::NO_EVENTS;
+	}
+
+	void displayDetails() {
+		std::cout << std::endl << "Event Name: " << this->getName();
+		std::cout << std::endl << "Date: " << this->getDate();
+		std::cout << std::endl << "Time: " << this->getTime();
+	}
+
+	Event(std::string name, const char* date, const char* time) {
+		this->setName(name);
+		this->setDate(date);
+		this->setTime(time);
+		Event::NO_EVENTS += 1;
+	}
 };
 
 int Event::NO_EVENTS = 0;
 
 class Person {
-	std::string name;
+	const char* name;
 	const char dateOfBirth[11]; //	dd/mm/yyyy
 	char gender;
 	std::string* email = nullptr;
@@ -33,6 +147,7 @@ class Ticket {
 	const Event& event;
 	const Location& location;
 	const Person& person;
+	float price = 0;
 	static int NO_TICKETS;
 public:
 };
--- TicketTango/main.cpp
+++ TicketTango/main.cpp
@@ -1,5 +1,9 @@
 #include "classes.h"
 
 int main() {
+	Location location("Cluj Arena", 30201, 15);
+	Event event("Untold Festival", "08/08/2024", "20:30");
 
+	location.displayDetails();
+	event.displayDetails();
 }
